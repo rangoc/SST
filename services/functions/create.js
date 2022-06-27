bdf536e4f2e8ffc46f024a4de89b17170c6187ed
rangoc <cabarkapa.goran@gmail.com>
2022-06-27 10:19:02 +0200

    refactor: refactor create note lambda

--- services/functions/create.js
+++ services/functions/create.js
@@ -1,9 +1,8 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "../util/handler";
+import dynamoDb from "../util/dynamodb";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event) {
+export const main = handler(async (event) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -19,17 +18,7 @@ export async function main(event) {
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+});
